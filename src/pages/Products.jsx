c7959f4cde83cffd75a90e4fc442422055a4b357
ersinn26 <154364329+ersinn26@users.noreply.github.com>
2024-05-14 22:24:50 +0300

    product

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,62 @@
+import { useEffect, useState } from "react"
+import useStockRequest from "../services/useStockRequest"
+import Typography from "@mui/material/Typography"
+import Button from "@mui/material/Button"
+import ProductModal from "../components/ProductModal"
+import ProductTable from "../components/ProductTable"
+import { useSelector } from "react-redux"
+import TableSkeleton, {
+  ErrorMessage,
+  NoDataMessage,
+} from "../components/DataFetchMessages"
+
 const Products = () => {
+  const { getStock } = useStockRequest()
+  const { error, loading } = useSelector((state) => state.stock)
+  const { products } = useSelector((state) => state.stock)
+
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => setOpen(true)
+
+  const initialState = { categoryId: "", brandId: "", name: "" }
+  const [info, setInfo] = useState(initialState)
+
+  const handleClose = () => {
+    setOpen(false)
+    setInfo(initialState)
+  }
+  useEffect(() => {
+    getStock("product")
+    getStock("categories")
+    getStock("brands")
+  }, [])
+
   return (
-    <div>Products</div>
+    <div>
+      <Typography variant="h4" color={"error"} mb={2}>
+        Products
+      </Typography>
+
+      <Button
+        variant="contained"
+        onClick={handleOpen}
+        sx={{ mb: 3 }}
+        disabled={error}
+      >
+        New Product
+      </Button>
+
+      {loading && <TableSkeleton />}
+      {error && <ErrorMessage />}
+      {!error && !loading && products.length > 0 && <ProductTable />}
+      {!error && !products.length && <NoDataMessage />}
+      <ProductModal
+        handleClose={handleClose}
+        open={open}
+        info={info}
+        setInfo={setInfo}
+      />
+    </div>
   )
 }
 
