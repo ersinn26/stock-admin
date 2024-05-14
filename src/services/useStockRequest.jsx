c3ab0dfbfb36c0bee2ebc3c5d62239de328733f6
ersinn26 <154364329+ersinn26@users.noreply.github.com>
2024-05-14 22:23:52 +0300

    req

--- src/services/useStockRequest.jsx
+++ src/services/useStockRequest.jsx
@@ -1,10 +1,17 @@
-import { useDispatch } from "react-redux";
-import useAxios from "./useAxios";
-import { fetchFail, fetchStart, getStockSuccess } from "../features/stockSlice";
+import { useDispatch } from "react-redux"
+import useAxios from "./useAxios"
+import {
+  fetchFail,
+  fetchStart,
+  getFirmsSuccess,
+  getSalesSuccess,
+  getStockSuccess,
+} from "../features/stockSlice"
+import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify"
 
 const useStockRequest = () => {
-  const { axiosToken } = useAxios();
-  const dispatch = useDispatch();
+  const { axiosToken } = useAxios()
+  const dispatch = useDispatch()
 
   // const getFirms = async () => {
   //   dispatch(fetchStart())
@@ -29,31 +36,57 @@ const useStockRequest = () => {
   // }
 
   const getStock = async (path = "firms") => {
-    dispatch(fetchStart());
+    dispatch(fetchStart())
     try {
-      const { data } = await axiosToken(`/${path}`);
-      const stockData = data.data;
-      dispatch(getStockSuccess({ stockData, path }));
+      const { data } = await axiosToken(`/${path}`)
+      const stockData = data.data
+      dispatch(getStockSuccess({ stockData, path }))
     } catch (error) {
-      dispatch(fetchFail());
-      console.log(error);
+      // toastErrorNotify(`${path} verileri çekilememiştir.`)
+      dispatch(fetchFail())
+      console.log(error)
     }
-  };
+  }
 
   const deleteStock = async (path = "firms", id) => {
-    dispatch(fetchStart());
+    dispatch(fetchStart())
     try {
-      await axiosToken.delete(`/${path}/${id}`);
-      getStock(path);
+      await axiosToken.delete(`/${path}/${id}`)
+      toastSuccessNotify(`${path} basariliyla silinmiştir.`)
+      getStock(path)
     } catch (error) {
-      dispatch(fetchFail());
-      console.log(error);
+      toastErrorNotify(`${path} silinememiştir.`)
+      dispatch(fetchFail())
+      console.log(error)
     }
-  };
+  }
 
+  const postStock = async (path = "firms", info) => {
+    dispatch(fetchStart())
+    try {
+      await axiosToken.post(`/${path}/`, info)
+      getStock(path)
+      toastSuccessNotify(`${path} basariliyla eklenmiştir.`)
+    } catch (error) {
+      dispatch(fetchFail())
+      toastErrorNotify(`${path} eklenememiştir.`)
+      console.log(error)
+    }
+  }
+
+  const putStock = async (path = "firms", info) => {
+    dispatch(fetchStart())
+    try {
+      await axiosToken.put(`/${path}/${info._id}`, info)
+      getStock(path)
+    } catch (error) {
+      dispatch(fetchFail())
+      console.log(error)
+    }
+  }
   // return { getFirms, getSales }
 
-  return { getStock, deleteStock };
-};
+  return { getStock, deleteStock, postStock, putStock }
+}
 
-export default useStockRequest;
+export default useStockRequest
