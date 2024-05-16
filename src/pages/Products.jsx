6cf1297a1f3aef53683a8546b5051936eaac9f87
ersinn26 <154364329+ersinn26@users.noreply.github.com>
2024-05-16 23:06:37 +0300

    pro add

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,35 +1,35 @@
-import { useEffect, useState } from "react"
-import useStockRequest from "../services/useStockRequest"
-import Typography from "@mui/material/Typography"
-import Button from "@mui/material/Button"
-import ProductModal from "../components/ProductModal"
-import ProductTable from "../components/ProductTable"
-import { useSelector } from "react-redux"
+import { useEffect, useState } from "react";
+import useStockRequest from "../services/useStockRequest";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import ProductModal from "../components/ProductModal";
+import ProductTable from "../components/ProductTable";
+import { useSelector } from "react-redux";
 import TableSkeleton, {
   ErrorMessage,
   NoDataMessage,
-} from "../components/DataFetchMessages"
+} from "../components/DataFetchMessages";
 
 const Products = () => {
-  const { getStock } = useStockRequest()
-  const { error, loading } = useSelector((state) => state.stock)
-  const { products } = useSelector((state) => state.stock)
+  const { getStock } = useStockRequest();
+  const { error, loading } = useSelector((state) => state.stock);
+  const { products } = useSelector((state) => state.stock);
 
-  const [open, setOpen] = useState(false)
-  const handleOpen = () => setOpen(true)
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
 
-  const initialState = { categoryId: "", brandId: "", name: "" }
-  const [info, setInfo] = useState(initialState)
+  const initialState = { categoryId: "", brandId: "", name: "" };
+  const [info, setInfo] = useState(initialState);
 
   const handleClose = () => {
-    setOpen(false)
-    setInfo(initialState)
-  }
+    setOpen(false);
+    setInfo(initialState);
+  };
   useEffect(() => {
-    getStock("product")
-    getStock("categories")
-    getStock("brands")
-  }, [])
+    getStock("product");
+    getStock("categories");
+    getStock("brands");
+  });
 
   return (
     <div>
@@ -57,7 +57,7 @@ const Products = () => {
         setInfo={setInfo}
       />
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
