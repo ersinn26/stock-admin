0f31ed0008928d4ae17f96d771f7e465a88d7534
ersinn26 <154364329+ersinn26@users.noreply.github.com>
2024-05-17 23:34:06 +0300

    firmcard

--- src/components/FirmCard.jsx
+++ src/components/FirmCard.jsx
@@ -1,20 +1,20 @@
-import * as React from "react"
-import Card from "@mui/material/Card"
-import CardActions from "@mui/material/CardActions"
-import CardContent from "@mui/material/CardContent"
-import CardMedia from "@mui/material/CardMedia"
-import Typography from "@mui/material/Typography"
-import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline"
-import EditIcon from "@mui/icons-material/Edit"
-import { btnStyle } from "../styles/globalStyles"
-import useStockRequest from "../services/useStockRequest"
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import EditIcon from "@mui/icons-material/Edit";
+import { btnStyle } from "../styles/globalStyles";
+import useStockRequest from "../services/useStockRequest";
 
-export default function FirmCard({ firm }) {
+export default function FirmCard({ firm, handleOpen, setInfo }) {
   //   const btnStyle = {
   //     "&:hover": { color: "red", cursor: "pointer" },
   //   }
-  const { address, _id, name, phone, image } = firm
-  const { deleteStock } = useStockRequest()
+  // const { address, _id, name, phone, image } = firm
+  const { deleteStock } = useStockRequest();
   return (
     <Card
       sx={{
@@ -28,25 +28,36 @@ export default function FirmCard({ firm }) {
         p: 2,
       }}
     >
-      <CardMedia component="img" alt={name} height="140" image={image} />
+      <CardMedia
+        component="img"
+        alt={firm?.name}
+        height="140"
+        image={firm?.image}
+      />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          {name}
+          {firm?.name}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          {address}
+          {firm?.address}
         </Typography>
         <Typography variant="body2" color="text.secondary" mt={2}>
-          {phone}
+          {firm?.phone}
         </Typography>
       </CardContent>
       <CardActions>
         <DeleteOutlineIcon
           sx={btnStyle}
-          onClick={() => deleteStock("firms", _id)}
+          onClick={() => deleteStock("firms", firm?._id)}
         />
-        <EditIcon sx={btnStyle} />
+        <EditIcon
+          sx={btnStyle}
+          onClick={() => {
+            handleOpen();
+            setInfo(firm);
+          }}
+        />
       </CardActions>
     </Card>
-  )
+  );
 }
