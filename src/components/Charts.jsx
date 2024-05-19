5c87b656714d6c0e7d704a4aebe7f139222549ae
ersinn26 <154364329+ersinn26@users.noreply.github.com>
2024-05-20 02:15:09 +0300

    charts-2

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,26 +1,26 @@
-import { AreaChart } from "@tremor/react"
-import { useSelector } from "react-redux"
+import { AreaChart } from "@tremor/react";
+import { useSelector } from "react-redux";
 
 const dataFormatter = (number) =>
-  `$${Intl.NumberFormat("us").format(number).toString()}`
+  `$${Intl.NumberFormat("us").format(number).toString()}`;
 
 const Charts = () => {
-  const { sales, purchases } = useSelector((state) => state.stock)
+  const { sales, purchases } = useSelector((state) => state.stock);
 
   const salesData = sales?.map((item) => ({
     date: new Date(item.createdAt).toLocaleDateString("tr-TR"),
     amount: item.amount,
-  }))
+  }));
 
   const purchasesData = purchases?.map((item) => ({
     date: new Date(item.createdAt).toLocaleDateString("tr-TR"),
     amount: item.amount,
-  }))
-  console.log(salesData)
+  }));
+  console.log(salesData);
   return (
-    <>
+    <div className="flex w-full flex-row items-center justify-center gap-10 mt-14 ">
       <AreaChart
-        className="h-80"
+        className="h-80 w-1/2 border-2 border-gray-400 rounded-lg "
         data={salesData}
         index="date"
         categories={["amount"]}
@@ -30,7 +30,7 @@ const Charts = () => {
       />
 
       <AreaChart
-        className="h-80"
+        className="h-80 w-1/2 border-2 border-gray-400 rounded-lg"
         data={purchasesData}
         index="date"
         categories={["amount"]}
@@ -38,7 +38,7 @@ const Charts = () => {
         valueFormatter={dataFormatter}
         yAxisWidth={60}
       />
-    </>
-  )
-}
-export default Charts
+    </div>
+  );
+};
+export default Charts;
